68af66df3a545bdf0ec37348b5fe556de2d6cc05
Darryl Dolan-Rathmell <darryl.dolan@hotmail.co.uk>
2019-06-11 14:55:53 +0100

    Variables SCSS file added.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,12 @@
 <template>
-    
+    <nav>
+        <ul>
+            <li>Facilities</li>
+            <li>Membership</li>
+            <li>Coaching</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 </template>
 <script>
 export default {
@@ -7,5 +14,27 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+@import "../scss/variables.scss";
+    nav ul {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        list-style-type: none;
+        padding: 0;
+        li {
+            a {
+            padding: 30px;
+            }
+            a:link {}
+            // Change to primary theme colour
+            a:hover {
+            color: $primary-colour;
+            cursor: pointer;
+            border-bottom: 1px solid $primary-colour;
+            }
+            a:active {
+            color: $primary-colour;
+            }
+        }
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <Nav />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Nav from "@/components/Nav.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Nav
   }
 };
 </script>
