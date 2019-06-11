ca4862401d2399ec4b6dd20b77cfee0ce8cb8491
Darryl Dolan-Rathmell <darryl.dolan@hotmail.co.uk>
2019-06-11 16:38:53 +0100

    Welcome component created.

---
+++ src/components/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="welcome">
+        <h1>Welcome to Elland Tennis Club</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at augue finibus tortor feugiat volutpat ut eget nulla. Nulla ut ex in dui suscipit pretium. Pellentesque dapibus justo eget ante tempor, et lacinia nunc malesuada. Maecenas sed odio eu sem vehicula tempus. Vestibulum sed quam ligula. Nunc dapibus sapien at mauris mollis laoreet. Donec sem arcu, ornare in felis ac, bibendum porta justo. Sed et enim lobortis, tempor justo in, porttitor est. In ornare orci sed auctor condimentum. Phasellus sollicitudin quam sed sodales tincidunt.</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "Welcome"
+}
+</script>
+<style lang="scss" scoped>
+    @import "../scss/variables.scss";
+    .welcome {
+        h1 {
+            font-family: $primary-heading;
+        }
+        p {
+            font-family: $primary-text;
+        }
+    }
+</style>
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <Nav />
+    <Welcome/>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <Footer/>
   </div>
@@ -11,13 +11,15 @@
 // @ is an alias to /src
 //import HelloWorld from "@/components/HelloWorld.vue";
 import Nav from "@/components/Nav.vue";
+import Welcome from "@/components/Welcome.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
   name: "home",
   components: {
     Nav,
-    Footer
+    Footer,
+    Welcome
   }
 };
 </script>
